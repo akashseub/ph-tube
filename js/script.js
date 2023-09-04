0eb708e10cfe89eeae4c030afc4a628c96e62967
akashseub <miahmdakash55@gmail.com>
2023-09-04 21:23:44 +0600

    complete

--- js/script.js
+++ js/script.js
@@ -23,8 +23,7 @@ const loadCategory = async () => {
 loadCategory();
 
 let data;
-let result1;
-console.log(result1,'result');
+
 const allData = async (category) => {
     console.log(category);
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${category}`);
@@ -39,8 +38,7 @@ const allData = async (category) => {
         notFound.classList.add('hidden');
     }
     
-    result = data.data;
-    result1 = data.data;
+    const result = data.data;
 
 
     const contentContainer = document.getElementById('content-container');
@@ -71,10 +69,7 @@ const allData = async (category) => {
                             </div>
                             <div>
                                 ${data.authors[0].verified === true ?  `
-                                    <i class="fa-solid fa-certificate text-[#2568ef]"></i>`
-                                   :
-                                   ""
-                                
+                                    <i class="fa-solid fa-certificate text-[#2568ef]"></i>`: ""
                                 }
                             </div>
                         </div>
@@ -102,23 +97,70 @@ function convertSecondsToTime(seconds) {
 }
 
 
-sortByButton.onclick = async function() {
 
-    let cont = data.data.sort((a, b) => {
-        const viewsA = parseInt(a.others.views.replace(/[^\d]/g, ''), 10); 
-        const viewsB = parseInt(b.others.views.replace(/[^\d]/g, ''), 10);
-    
-        return viewsB - viewsA;
+
+function renderData(dataItem) {
+    const contentContainer = document.getElementById('content-container');
+    contentContainer.textContent = '';
+
+    dataItem.forEach(dataItem => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <div class="card w-96 bg-base-100">
+            <figure class="pb-4">
+                <img src="${dataItem.thumbnail}" alt="Shoes" class="h-60 rounded-xl relative" />
+                ${dataItem.others.posted_date !== '' ? `
+                <div class="absolute right-4 bottom-28 text-white text-xs bg-black p-1 rounded-lg">
+                    <p>${convertSecondsToTime(dataItem.others.posted_date)}</p>
+                </div>` : ''}
+            </figure>
+            <div class="flex space-x-2">
+                <div>
+                    <img class = 'h-12 w-12 rounded-full' src="${dataItem.authors[0].profile_picture}" alt="">
+                </div>
+                <div class = "space-y-2">
+                    <div>
+                        <h1 class = "text-xl font-bold">${dataItem.title}</h1>
+                    </div>
+                    <div class = "flex space-x-4">
+                        <div>
+                            <h4 class = "text-gray-500">${dataItem.authors[0].profile_name}</h4>
+                        </div>
+                        <div>
+                            ${dataItem.authors[0].verified === true ?  `
+                                <i class="fa-solid fa-certificate text-[#2568ef]"></i>`: ""
+                            }
+                        </div>
+                    </div>
+                    <div>
+                        <p class = "text-gray-500">${dataItem.others.views}</p>
+                    </div>
+                </div>
+            </div>
+            </div>
+        `;
+        contentContainer.appendChild(div);
     });
+}
 
-    result = cont;
-    
-    console.log(cont);
 
-    
-   
-    console.log("Sorting by view...");
-};
+function sortByViews(a, b) {
+    const viewsA = parseInt(a.others.views.replace(/[^\d]/g, ''), 10);
+    const viewsB = parseInt(b.others.views.replace(/[^\d]/g, ''), 10);
+
+    return viewsB - viewsA;
+}
 
 
 allData(1000);
+
+
+sortByButton.onclick = function () {
+    if (data && data.data) {
+        const sortedData = data.data.slice(); 
+        sortedData.sort(sortByViews); 
+
+        
+        renderData(sortedData);
+    }
+};
